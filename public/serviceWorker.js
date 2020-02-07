93ca5717c0f3ca59b836e66c25cb0e52840dbf76
Zubair Bashir <zubair.bashirzubair@gmail.com>
2020-02-07 12:14:04 +0530

    Implmented route based caching

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,64 +1,164 @@
-const APP_SHELL_CACHE = 'appShell-v3'
-const DYNAMIC_CACHE = 'dynamic-v3'
+/*-----------------------------------------------------CONSTANTS---------------------------------------------------------- */
+const APP_SHELL_CACHE = 'appShell-v14'
+const DYNAMIC_CACHE = 'dynamicData';
+const APP_SHELL_ASSETS = [
+    '/',
+    '/index.html',
+    '/offline.html',
+    '/src/js/app.js',
+    '/src/js/feed.js',
+    '/src/js/material.min.js',
+    '/src/css/app.css',
+    '/src/css/feed.css',
+    '/src/images/main-image.jpg',
+    'https://fonts.googleapis.com/css?family=Roboto:400,700',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+];
+const feedURL = 'https://httpbin.org/get';
+/*----------------------------------------------------------------------------------------------------------------------- */
 
+/*-------------------------------------------------EVENT LISTENERS------------------------------------------------------- */
 self.addEventListener('install',(event)=>{
-    console.log("[Service Worker]: Installing Service Worker...");
 
-    console.log("[Service Worker]: Pre-Caching the App Shell...")
     event.waitUntil( handlePreCaching() );/* remain in the installation phase until the pre-caching is done */
 })
 
 self.addEventListener('activate',(event)=>{
-    console.log("[Service Worker]: Activating Service Worker...");
-
-    console.log("[Service Worker]: Performing Cache cleanup...")
+    
     event.waitUntil(handleCacheCleanUp());/* remain in the activation phase until the cache-cleanup is done */
 })
 
 self.addEventListener('fetch',(event)=>{
-    
-    event.respondWith(
-        handleFetch(event.request).catch(err=>
-            console.log("[Service Worker]: An error occurred in Fetch ",err)
-        )
-    );
+    const assetRequest = event.request;
+    if(requestContainsURL(assetRequest,[feedURL])){
+        event.respondWith(
+            handleNetworkThenCache(assetRequest).catch(()=> handleFailedFetch(assetRequest))
+        );
+    }else if(requestContainsURL(assetRequest,APP_SHELL_ASSETS)){
+        event.respondWith(
+            handleFetchCacheOnly(assetRequest).catch(()=> handleFailedFetch(assetRequest))
+        );
+    }else{
+        event.respondWith(
+            handleFetchCWNF(assetRequest).catch(()=> handleFailedFetch(assetRequest))
+        );
+    }
     
 })
+/*--------------------------------------------------------------------------------------------------------------------- */
 
+
+/*-------------------------------------------------UTILITY FUNCTIONS------------------------------------------------------- */
 async function handlePreCaching(){
     const appShellCache = await caches.open(APP_SHELL_CACHE);
-    appShellCache.addAll([
-        '/',
-        '/index.html',
-        '/src/js/app.js',
-        '/src/js/feed.js',
-        '/src/js/material.min.js',
-        '/src/css/app.css',
-        '/src/css/feed.css',
-        '/src/images/main-image.jpg',
-        'https://fonts.googleapis.com/css?family=Roboto:400,700',
-        'https://fonts.googleapis.com/icon?family=Material+Icons',
-        'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
-    ]);
+    return appShellCache.addAll(APP_SHELL_ASSETS);
 }
 
 async function handleCacheCleanUp(){
     const existingCaches = await caches.keys();
     existingCaches.forEach(async(cache)=>{
-        if(cache !== APP_SHELL_CACHE && cache !== DYNAMIC_CACHE){
+        if(cache !== APP_SHELL_CACHE ){
             await caches.delete(cache);
         }
     })
 }
 
-async function handleFetch(request){
+async function handleFetchCWNF(request){
     const cacheResponse = await caches.match(request);
     if(cacheResponse)
         return cacheResponse;
     else{
         const networkResponse = await fetch(request);
         const dynamicCache = await caches.open(DYNAMIC_CACHE);
+        // trimCache(DYNAMIC_CACHE,10);
         dynamicCache.put(request.url,networkResponse.clone());
+        // showStorageDetails();
         return networkResponse;
     }
+}
+
+async function handleFetchNWCF(request){
+    try{
+        const networkResponse = await fetch(request);
+        const dynamicCache = await caches.open(DYNAMIC_CACHE);
+        // trimCache(DYNAMIC_CACHE,10);
+        dynamicCache.put(request.url,networkResponse.clone());
+        return networkResponse;
+    }catch(err){
+        const cacheResponse = await caches.match(request);
+        if(cacheResponse){
+            return cacheResponse;
+        }
+        else{
+            throw new Error('Response could not be retrieved!');
+        }
+    }
+}
+
+async function handleFetchCacheOnly(request){
+    const cacheResponse = await caches.match(request);
+    if(cacheResponse)
+        return cacheResponse;
+    else{
+        throw new Error('Response not found in the cache!');
+    }
+}
+
+async function handleFetchNetworkOnly(request){
+    return fetch(request);
+}
+
+async function handleNetworkThenCache(request){
+    const networkResponse = await fetch(request);
+    const dynamicCache = await caches.open(DYNAMIC_CACHE);
+    // trimCache(DYNAMIC_CACHE,10);
+    dynamicCache.put(request,networkResponse.clone());
+    return networkResponse;
+}
+
+async function handleFailedFetch(request){
+    if(request.headers.get('accept').includes('text/html')){
+        const appShellCache = await caches.open(APP_SHELL_CACHE);
+        const fallBackPage = await appShellCache.match('/offline.html');
+        return fallBackPage;
+    }else{
+        return null;
+    }
+}
+
+async function trimCache(cacheName, numberOfItemsToKeep){
+    const cacheToTrim  = await caches.open(cacheName);
+    const cachedItems  = await cacheToTrim.keys();
+    let currentNumberOfItems = cachedItems.length;
+    let i = 0;
+    while( currentNumberOfItems > numberOfItemsToKeep ){
+        await cacheToTrim.delete(cachedItems[i++]);
+        currentNumberOfItems--;
+    }
+}
+
+async function showStorageDetails(){
+    if( navigator.storage && navigator.storage.estimate ){
+        const {
+            quota : allocatedStorage,
+            usage : usedStorage,
+            usageDetails } = await navigator.storage.estimate();
+
+        const percentUsed = (usedStorage/allocatedStorage*100);
+        console.log(`You've used ${percentUsed} of the available storage.Here are the details: `,usageDetails);
+    }
+}
+
+function constructRegexTest(pathList){
+    let regexString = "";
+    for (let i = 0; i < pathList.length; i++) {
+        regexString += pathList[i].replace(/\//gi,"\\/").replace(/\?/gi,"\\?").replace(/\+/gi,"\\+") + (i === pathList.length-1?"$" : "$|");
+    }
+    return new RegExp(regexString);
+}
+
+function requestContainsURL(assetRequest,listOfUrl){
+    const assetPathMatcher = constructRegexTest(listOfUrl);
+    return assetPathMatcher.test(assetRequest.url);
 }
--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -23,6 +23,16 @@ function showPromotion(){
 
 function openCreatePostModal() {
   createPostArea.style.display = 'block';
+  if('serviceWorker' in navigator){
+    removeServiceWorkers();
+  }
+}
+
+async function removeServiceWorkers(){
+  const registeredServiceWorkers = await navigator.serviceWorker.getRegistrations();
+  registeredServiceWorkers.forEach(async(serviceWorker)=>{
+    await serviceWorker.unregister();
+  })
 }
 
 function closeCreatePostModal() {
@@ -37,7 +47,14 @@ shareImageButton.addEventListener('click', openCreatePostModal);
 
 closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
 
+function clearCards(){
+  while(sharedMomentsArea.hasChildNodes()){
+    sharedMomentsArea.removeChild(sharedMomentsArea.lastChild);
+  }
+}
+
 function createCard() {
+  console.log("someone called me!");
   var cardWrapper = document.createElement('div');
   cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp';
   var cardTitle = document.createElement('div');
@@ -55,15 +72,58 @@ function createCard() {
   cardSupportingText.className = 'mdl-card__supporting-text';
   cardSupportingText.textContent = 'In San Francisco';
   cardSupportingText.style.textAlign = 'center';
+  // var cardSaveButton = document.createElement('button');
+  // cardSaveButton.textContent = 'SAVE'
+  // cardSaveButton.addEventListener('click',onSaveButtonClick)
+  // cardSupportingText.append(cardSaveButton);
   cardWrapper.appendChild(cardSupportingText);
   componentHandler.upgradeElement(cardWrapper);
   sharedMomentsArea.appendChild(cardWrapper);
 }
 
-fetch('https://httpbin.org/get') //fetching arbitrary data
-  .then(function(res) {
-    return res.json();
-  })
-  .then(function(data) { //creating a hard coded card
-    createCard();
-  });
+//cache-on-demand for the feed card
+// async function onSaveButtonClick(event){
+//   console.log('Save button clicked!');
+//   const userRequestedCache = await caches.open('userRequested');
+//   userRequestedCache.addAll(['/src/images/sf-boat.jpg','https://httpbin.org/get']);
+// }
+
+const dataURL = 'https://httpbin.org/get';
+var cardCreatedFromNetworkResponse = false; 
+cacheThenNetworkFetch(dataURL);
+
+function cacheThenNetworkFetch(dataURL){
+  fetchFromCache(dataURL);
+  fetchFromNetwork(dataURL);
+}
+
+async function fetchFromCache(dataURL){
+  console.log("[Application]: Fetching the card Data from the cache...");
+  const cacheResponse = await caches.match(dataURL)
+  if(cacheResponse){
+    const responseData = await cacheResponse.json();
+    if (cardCreatedFromNetworkResponse) {
+      console.log("Cache response received but card already created using network response.")
+      return;
+    } else {
+      console.log("Creating the card from cache response");
+      createCard(responseData);
+    }
+  }else{
+    console.log("[Application]: Fetching from the cache failed...")
+  }
+}
+
+async function fetchFromNetwork(dataURL){
+  console.log("[Application]: Fetching the card Data from the network");
+  try{
+    const networkResponse = await fetch(dataURL);
+    const responseData = networkResponse.json();
+    console.log("[Application]: Creating CARD from network data...")
+    cardCreatedFromNetworkResponse = true;
+    clearCards(); //instead of appending a new card replaces the older card which was created from the cache
+    createCard(responseData);
+  }catch(err){
+    console.log("Oops! Network fetch failed!");
+  }
+}
