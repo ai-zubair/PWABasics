b2799292e7eab058977f02a187b19a2b971a3d25
Zubair Bashir <zubair.bashirzubair@gmail.com>
2020-04-07 17:16:58 +0530

    Add functionality for Remove subscription

--- functions/utility.js
+++ functions/utility.js
@@ -6,11 +6,15 @@ function fetchPushSubscriptions(){
     return admin.database().ref('pushSubscriptions').once("value");
 }
 
+async function removePushSubscription(subscriptionKey){
+    await admin.database().ref(`/pushSubscriptions/${subscriptionKey}`).remove();
+}
+
 function sendNotification(subscription, data){
-    return webpush.sendNotification(subscription,JSON.stringify(data)).catch(err=>{
+    return webpush.sendNotification(subscription.val(),JSON.stringify(data)).catch(err=>{
         if (err.statusCode === 404 || err.statusCode === 410) {
             console.log('Subscription has expired or is no longer valid: ', err);
-            // return deleteSubscriptionFromDatabase(subscription._id);
+            removePushSubscription(subscription.key);
           }else{
             throw err;
           }
@@ -18,13 +22,12 @@ function sendNotification(subscription, data){
 }
 
 async function asyncWrapper(subscription,notificationPayload){
-    const pushServiceResponse = await sendNotification(subscription.val(),notificationPayload);
+    const pushServiceResponse = await sendNotification(subscription,notificationPayload);
     console.log(pushServiceResponse);
 }
 async function sendPushNotification( notificationPayload ){
     webpush.setVapidDetails('mailto:zubair.bashirzubair@gmail.com',vapidKeys.publicKey,vapidKeys.privateKey); //IDENTIFY THE APPLICATION SERVER
     const pushSubscriptions = await fetchPushSubscriptions(); //fetch PS datasnapshot
-    console.log(pushSubscriptions.val());
     pushSubscriptions.forEach( (subscription)=>{
         asyncWrapper(subscription,notificationPayload);
     });
