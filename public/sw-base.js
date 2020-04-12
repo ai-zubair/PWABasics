53216f96fde8ee9f30f87d52f83a0aa80246a064
Zubair Bashir <zubair.bashirzubair@gmail.com>
2020-04-13 00:51:21 +0530

    Add fallback for offline

--- public/sw-base.js
+++ public/sw-base.js
@@ -4,6 +4,8 @@ importScripts('/utils/assets.js');
 importScripts('/utils/iDButils.js');
 importScripts('/utils/sw_utils.js');
 
+workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+
 const cacheExpiration = new workbox.expiration.ExpirationPlugin({
     maxAgeSeconds: 60*60*24*5,
     maxEntries: 10,
@@ -31,9 +33,23 @@ const SWR_FBS = new workbox.strategies.StaleWhileRevalidate({
     ]
 }) 
 
+const fallBackRoute = new workbox.routing.Route((matchContext)=>{
+    if(matchContext.request.headers.get('accept').includes('text/html')){
+        return true;
+    }
+},async(handleContext)=>{
+    try{
+        debugger;
+        const response = await sw_utils.handleFetchCWNF(handleContext.request);
+        return response;
+    }catch(err){
+        return caches.match(workbox.precaching.getCacheKeyForURL('/404.html'));
+        
+    }
+})
+
 workbox.routing.registerRoute(/.*(?:firebasestorage\.googleapis)\.com.*$/,SWR_FBS)
 workbox.routing.registerRoute(/.*(?:googleapis|gstatic)\.com.*$/,SWR_APIS)
 workbox.routing.registerRoute("https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css",SWR_MD)
 workbox.routing.registerRoute("https://pwabasics-199ce.firebaseio.com/posts.json",(context)=>{sw_utils.handleCacheThenNetwork(context.request)});
-
-workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+workbox.routing.registerRoute(fallBackRoute);
